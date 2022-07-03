3ab35b821973cce38a55111f37f8fefa0de0f103
Youp <youpkuiper@gmail.com>
2022-07-03 23:41:09 +0200

    Added Github Actions to build, install, zip and deploy lambda code

--- src/handler.ts
+++ src/handler.ts
@@ -1,4 +1,4 @@
-export const handler = async (event, context)=> {
+export const handler = async (event: any, context: any)=> {
     console.log(`Event: ${JSON.stringify(event, null, 2)}`);
     console.log(`Context: ${JSON.stringify(context, null, 2)}`);
 
