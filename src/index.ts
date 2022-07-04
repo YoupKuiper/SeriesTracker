a89f4beafb9e9b5c571529cfdf71a63bc506fbbd
Youp <youpkuiper@gmail.com>
2022-07-04 20:16:44 +0200

    Getting all tracked series airing today and sending a test email using amazon ses

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,52 @@
 const axios = require('axios').default;
+var aws = require("aws-sdk");
+var ses = new aws.SES({ region: process.env.AWS_REGION });
 
 export const handler = async (event: any, context: any)=> {
     console.log(`Event: ${JSON.stringify(event, null, 2)}`);
     console.log(`Context: ${JSON.stringify(context, null, 2)}`);
 
-    // Call movieDB to get todays airing series
+    // Hardcoded list of series to track
+    const seriesToTrack = [202740, 133985, 107116, 205118]
 
     try {
+        // Call movieDB to get todays airing series
         const response = await axios.get(`https://api.themoviedb.org/3/tv/airing_today?api_key=${process.env.THE_MOVIE_DB_TOKEN}&language=en-US&page=1`);
-        console.log(response);
+
+        let promises = [];
+
+        console.log('Total pages of series airing today: ' + response.data.total_pages)
+
+        for (let pageNumber = 1; pageNumber < response.data.total_pages+1; pageNumber++){
+            promises.push(getSeriesForPage(pageNumber))
+        }
+
+        const allSeriesAiringTodayPaged = await Promise.all(promises);
+        const allSeriesAiringToday = Array.prototype.concat.apply([], allSeriesAiringTodayPaged);
+
+        const interestingSeriesAiringToday = allSeriesAiringToday.filter(series => {
+            return seriesToTrack.includes(series.id)
+        })
+        
+        console.log(interestingSeriesAiringToday)
+
+        // Send emails for every tracked series thats airing today
+        var params = {
+            Destination: {
+              ToAddresses: ["hansklok@endtest-mail.io"],
+            },
+            Message: {
+              Body: {
+                Text: { Data: "Test" },
+              },
+        
+              Subject: { Data: "Test Email" },
+            },
+            Source: "SeriesTrackerApplicationEmailAddress@gmail.com",
+          };
+         
+          return ses.sendEmail(params).promise()
+
       } catch (error) {
         console.error(error);
       }
@@ -22,3 +60,9 @@ export const handler = async (event: any, context: any)=> {
         }),
     };
 };
+
+
+const getSeriesForPage = async (pageNumber: number) => {
+    const response = await axios.get(`https://api.themoviedb.org/3/tv/airing_today?api_key=${process.env.THE_MOVIE_DB_TOKEN}&language=en-US&page=${pageNumber}`)
+    return response.data.results;
+}
