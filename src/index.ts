a0f2c3eb1852c2ed30d9ee045deb26fe33a866f6
Youp <youpkuiper@gmail.com>
2022-07-04 16:06:55 +0200

    Added env var for movie token, using github secret to get token, calling movie api on lambda fire

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+const axios = require('axios').default;
+
 export const handler = async (event: any, context: any)=> {
     console.log(`Event: ${JSON.stringify(event, null, 2)}`);
     console.log(`Context: ${JSON.stringify(context, null, 2)}`);
 
     // Call movieDB to get todays airing series
 
+    try {
+        const response = await axios.get('https://api.themoviedb.org/3/tv/airing_today?&language=en-US&page=1',
+        {
+            headers: {'Authorization': process.env.THE_MOVIE_DB_TOKEN},
+        });
+        console.log(response);
+      } catch (error) {
+        console.error(error);
+      }
 
     // Call SNS To send Email?
 
