844bf79a175274d1659905bf44cf3c7692720869
Youp <youpkuiper@gmail.com>
2022-07-11 16:47:36 +0200

    checking dynamodb getuser for login lambda

--- src/LoginLambda.ts
+++ src/LoginLambda.ts
@@ -1,3 +1,27 @@
+import aws from "aws-sdk";
+import { isCorrectPassword } from "./lib/passwordHelper";
+const dynamoDB = new aws.DynamoDB({ region: process.env.AWS_REGION });
+
 export const handler = async (event: any, context: any)=> {
-    throw new Error('Method not implemented.');
+    console.log(`Incoming event body: ${JSON.stringify(event.body)}`);
+
+    try {
+        // Get user from DynamoDB by EmailAddress
+        const user = await dynamoDB.getItem({
+            Key: aws.DynamoDB.Converter.marshall({
+                "EmailAddress": event.body.username
+            }),
+            TableName: process.env.USER_TABLE_NAME || '',
+        }).promise() || {}
+
+        console.log(`USER: ${JSON.stringify(user)}`)
+
+        // // Check if password is correct
+        // if(await isCorrectPassword(event.body.password, user.Item?.hashedPassword)){
+        //     // Endcode a JWT token and return
+        // }
+    } catch (error) {
+        console.log(error)
+    }
+
 }
--- src/lib/passwordHelper.ts
+++ src/lib/passwordHelper.ts
@@ -3,3 +3,8 @@ import bcrypt from 'bcrypt'
 export const createPasswordHash = async (plainTextPassword: string) => {
     return await bcrypt.hash(plainTextPassword, 10)
 }
+
+
+export const isCorrectPassword = async (password: string, hashedPassword: string) => {
+    return !!await bcrypt.compare(password, hashedPassword)
+}
