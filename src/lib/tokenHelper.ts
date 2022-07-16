2c6e64fa0c93e95127b55d9d615e8721aff19b0a
Youp <youpkuiper@gmail.com>
2022-07-16 23:23:15 +0200

    added issued at to jwt token

--- src/lib/tokenHelper.ts
+++ src/lib/tokenHelper.ts
@@ -2,8 +2,9 @@ import jwt from 'jsonwebtoken';
 
 export const signTokenFor = (emailAddress: string) => {
     return jwt.sign({
-        exp: Math.floor(Date.now() / 1000) + (60 * 60), //expire after 1 hour
-        data: { emailAddress: emailAddress }
+        exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 7), //expire after 1 week
+        data: { emailAddress: emailAddress },
+        iat: Math.floor(Date.now() / 1000)
       }, process.env.JWT_SECRET || '');
 }
 
