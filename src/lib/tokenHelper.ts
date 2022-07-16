bcdc8d71e1ad60dc65ead398bb989251e9d54b05
Youp <youpkuiper@gmail.com>
2022-07-16 23:54:17 +0200

    checking if env var is there before doing token actions, throw error if its not there

--- src/lib/tokenHelper.ts
+++ src/lib/tokenHelper.ts
@@ -1,15 +1,21 @@
 import jwt from 'jsonwebtoken';
 
 export const signTokenFor = (emailAddress: string) => {
-    return jwt.sign({
-        exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 7), //expire after 1 week
-        data: { emailAddress: emailAddress },
-        iat: Math.floor(Date.now() / 1000)
-      }, process.env.JWT_SECRET || '');
+    if(process.env.JWT_SECRET){
+        return jwt.sign({
+            exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 7), //expire after 1 week
+            data: { emailAddress: emailAddress },
+            iat: Math.floor(Date.now() / 1000)
+          }, process.env.JWT_SECRET);    
+    }
+    throw new Error(`Environment variable for JWT secret required`);
 }
 
 export const isValid = (token: string): any => {
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET || '')
-    console.log(`Decoded token: ${JSON.stringify(decodedToken)}`)
-    return decodedToken;
+    if(process.env.JWT_SECRET){
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET || '')
+        console.log(`Decoded token: ${JSON.stringify(decodedToken)}`)
+        return decodedToken;
+    }
+    throw new Error(`Environment variable for JWT secret required`);
 }
