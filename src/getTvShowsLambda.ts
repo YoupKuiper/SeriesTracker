9ba58e44746ba5744b38d862c2abed8ba94ceaed
Youp <youpkuiper@gmail.com>
2022-07-23 16:46:24 +0200

    added handler for getting tv shows, renamed tvshowstable resource

---
+++ src/getTvShowsLambda.ts
@@ -0,0 +1,49 @@
+import aws from "aws-sdk";
+import { isCorrectPassword } from "./lib/passwordHelper";
+import { sendErrorResponse, sendOKResponse } from "./lib/responseHelper";
+import { signTokenFor } from "./lib/tokenHelper";
+const dynamoDB = new aws.DynamoDB({ region: process.env.AWS_REGION });
+
+export const handler = async (event: any, context: any)=> {
+    console.log(`Incoming event body: ${JSON.stringify(event.body)}`);
+
+    const parsedEvent = JSON.parse(event.body);
+    console.log(`Parsed event body: ${JSON.stringify(parsedEvent)}`);
+
+    const { password, emailAddress } = parsedEvent
+
+    console.log(`Email address: ${emailAddress}`)
+
+    try {
+        // Get user from DynamoDB by EmailAddress
+        const userDto = await dynamoDB.getItem({
+            Key: aws.DynamoDB.Converter.marshall({
+                "emailAddress": emailAddress
+            }),
+            TableName: process.env.USER_TABLE_NAME || '',
+        }).promise()
+
+        if(!userDto.Item){
+            return sendErrorResponse('Invalid credentials');
+        }
+
+        const { hashedPassword, ...user } = aws.DynamoDB.Converter.unmarshall(userDto.Item)
+
+        // Check if password is correct
+        if(await isCorrectPassword(password, hashedPassword)){
+            // Endcode a JWT token and return
+            if(process.env.JWT_SECRET){
+                const token = signTokenFor(user.emailAddress)
+                
+                return sendOKResponse({ token, user });
+            }
+            throw new Error(`Environment variable for JWT secret required`);
+        }
+        
+        return sendErrorResponse('Invalid credentials');
+    } catch (error) {
+        console.error(error)
+        return sendErrorResponse('Failed log in user')
+    }
+
+}
