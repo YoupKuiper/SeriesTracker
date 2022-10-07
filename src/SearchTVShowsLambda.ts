8e8bd76861c4b6eb58a98e295370d16baa9cbea7
Youp Kuiper <youpkuiper@gmail.com>
2022-10-07 09:18:55 +0700

    Returning similar shows

--- src/SearchTVShowsLambda.ts
+++ src/SearchTVShowsLambda.ts
@@ -59,6 +59,6 @@ const searchTVShows = async (title: number) => {
 }
 
 const getDetailsForTVShow = async (id: number) => {
-    const response = await axios.get(`https://api.themoviedb.org/3/tv/${id}?api_key=${process.env.THE_MOVIE_DB_TOKEN}`)
+    const response = await axios.get(`https://api.themoviedb.org/3/tv/${id}?api_key=${process.env.THE_MOVIE_DB_TOKEN}&append_to_response=similar`)
     return response.data;
 }
