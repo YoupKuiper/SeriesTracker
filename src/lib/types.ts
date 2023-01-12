9810b376f2905fc84b36b8e8b3a9d1e803488132
Youp Kuiper <youpkuiper@gmail.com>
2023-01-12 16:12:40 +0700

    Additional options added to zod type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,35 +1,39 @@
 import { z } from "zod";
 
 export const TVShowObject = z.object({
-    id: z.number(),
-    name: z.string(),
-    poster_path: z.string(),
-    genre_ids: z.array(z.number()).optional(),
-    origin_country: z.array(z.string()).optional(),
-    original_language: z.string().optional(),
-    original_name: z.string().optional(),
-    popularity: z.number(),
-    first_air_date: z.string().optional(),
-    overview: z.string(),
-    backdrop_path: z.string().optional(),
-    vote_average: z.number(),
-    vote_count: z.number()
-})
+	id: z.number(),
+	name: z.string(),
+	poster_path: z.string(),
+	genre_ids: z.array(z.number()).optional(),
+	origin_country: z.array(z.string()).optional(),
+	original_language: z.string().optional(),
+	original_name: z.string().optional(),
+	popularity: z.number(),
+	first_air_date: z.string().optional(),
+	overview: z.string(),
+	backdrop_path: z.string().optional(),
+	vote_average: z.number(),
+	vote_count: z.number(),
+});
 
 export const UserObject = z.object({
-    emailAddress: z.string(),
-    unsubscribeEmailToken: z.string(),
-    resetPasswordToken: z.string(),
-    emailAddressVerified: z.boolean(),
-    wantsEmailNotifications: z.boolean(),
-    trackedTVShows: z.array(TVShowObject),
-    hashedPassword: z.string(),
-})
+	emailAddress: z.string(),
+	unsubscribeEmailToken: z.string(),
+	resetPasswordToken: z.string(),
+	emailAddressVerified: z.boolean(),
+	wantsEmailNotifications: z.boolean(),
+	trackedTVShows: z.array(TVShowObject),
+	hashedPassword: z.string(),
+});
 
-export const UserUpdateObject = z.object({
-    wantsEmailNotifications: z.boolean().optional(),
-    trackedTVShows: z.array(TVShowObject).optional()
-}).strict();
+export const UserUpdateObject = z
+	.object({
+		wantsEmailNotifications: z.boolean().optional(),
+		mobileNotificationsToken: z.string().optional(),
+		wantsMobileNotifications: z.boolean().optional(),
+		trackedTVShows: z.array(TVShowObject).optional(),
+	})
+	.strict();
 
 export type User = z.infer<typeof UserObject>;
 export type TVShow = z.infer<typeof TVShowObject>;
